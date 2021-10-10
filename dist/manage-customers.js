f0f51d75b77994142eec6cd3763854a216c7617f
cisco <sovisrushain@gmail.com>
2021-10-10 15:23:53 +0530

    Load all customers functionality created

--- dist/manage-customers.js
+++ dist/manage-customers.js
@@ -1,14 +1,31 @@
-"use strict";
-/*===== Save Customer =====*/
-$('#btn-save').on('click', (e) => {
-    e.preventDefault();
-    const txtId = $('#txtId');
-    const txtName = $('#txtName');
-    const txtAddress = $('#txtAddress');
-    let id = txtId.val().trim();
-    let name = txtName.val().trim();
-    let address = txtAddress.val().trim();
-    console.log("Id ", id);
-    console.log("name ", name);
-    console.log("address ", address);
-});
+const BASE_API = 'http://localhost:8080/pos';
+const CUSTOMER_SERVICE_API = `${BASE_API}/customers`;
+let customers = [];
+let totalCustomers = 0;
+loadAllCustomers();
+/* load all customers */
+function loadAllCustomers() {
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+        if (http.readyState === http.DONE) {
+            if (http.status !== 200) {
+                alert('Failed to fetch customers, try again..!');
+                return;
+            }
+            customers = JSON.parse(http.responseText);
+            $('#tblCustomers tbody tr').remove();
+            customers.forEach((c) => {
+                const rowHtml = `<tr>
+                                    <td>${c.id}</td>
+                                    <td>${c.name}</td>
+                                    <td>${c.address}</td>
+                                    <td><i class="fas fa-trash"></i></td>
+                                </tr>`;
+                $('#tblCustomers tbody').append(rowHtml);
+            });
+        }
+    };
+    http.open('GET', CUSTOMER_SERVICE_API + '?page=1&size=6', true);
+    http.send();
+}
+export {};
--- src/manage-customers.ts
+++ src/manage-customers.ts
@@ -1,19 +1,40 @@
+import { Customer } from "./dto/customer.js";
 
-/*===== Save Customer =====*/
+const BASE_API = 'http://localhost:8080/pos';
+const CUSTOMER_SERVICE_API = `${BASE_API}/customers`;
 
-$('#btn-save').on('click', (e) => {
-    e.preventDefault();
+let customers: Array<Customer> = [];
+let totalCustomers = 0;
+loadAllCustomers();
 
-    const txtId = $('#txtId');
-    const txtName = $('#txtName');
-    const txtAddress = $('#txtAddress');
 
-    let id = (txtId.val() as string).trim();
-    let name = (txtName.val() as string).trim();
-    let address = (txtAddress.val() as string).trim();
+/* load all customers */
 
-    console.log("Id ", id);
-    console.log("name ", name);
-    console.log("address ", address); 
-})
+function loadAllCustomers() {
 
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = () => {
+        if(http.readyState === http.DONE){
+            if(http.status !== 200){
+                alert('Failed to fetch customers, try again..!');
+                return;
+            }
+            customers = JSON.parse(http.responseText); 
+            $('#tblCustomers tbody tr').remove();
+            customers.forEach((c) => {
+                const rowHtml = `<tr>
+                                    <td>${c.id}</td>
+                                    <td>${c.name}</td>
+                                    <td>${c.address}</td>
+                                    <td><i class="fas fa-trash"></i></td>
+                                </tr>`
+                $('#tblCustomers tbody').append(rowHtml);
+            });
+        }
+    }
+
+    http.open('GET', CUSTOMER_SERVICE_API + '?page=1&size=6', true);
+
+    http.send();
+}
