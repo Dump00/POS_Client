db3057ebdfe18d6316e6e0d2f8f70831afab2c93
cisco <sovisrushain@gmail.com>
2021-10-11 09:16:13 +0530

    Dashboard customer count display

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
-const income = $('#income-value').html();
+const BASE_API = 'http://localhost:8080/pos';
+const CUSTOMER_SERVICE_API = `${BASE_API}/customers`;
 
+let totalCustomers = 0;
+
+loadDetails();
+
+function loadDetails(): void {
+
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = () => {
+        if(http.readyState === http.DONE){
+            if(http.status !== 200) return;
+            totalCustomers = +(http.getResponseHeader('X-Total-Count'))!;
+            
+            
+            ($('#customer-count').html(totalCustomers+''));
+            
+        }
+    }
+
+    http.open('GET', CUSTOMER_SERVICE_API + `?page=${1}&size=${6}`, true);
+
+    http.send();
+}
+ 
 
 
 
