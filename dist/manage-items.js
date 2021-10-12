ea2b4959913c01b55dc6b86d3cb1a6fdb4910362
cisco <sovisrushain@gmail.com>
2021-10-12 10:04:54 +0530

    Items Pagination created

--- dist/manage-items.js
+++ dist/manage-items.js
@@ -25,9 +25,47 @@ function loadAllItems() {
                                 </tr>`;
                 $('#tblItems tbody').append(rowHtml);
             });
+            initPagination();
         }
     };
     http.open('GET', ITEM_SERVICE_API + `?page=${selectedPage}&size=${PAGE_SIZE}`, true);
     http.send();
+}
+/* pagination */
+function initPagination() {
+    console.log(totalItems);
+    pageCount = Math.ceil(totalItems / PAGE_SIZE);
+    showOrHidePagination();
+    if (pageCount == 1)
+        return;
+    let html = `<li class="page-item"><a class="page-link" href="javascript:void(0);">Previous</a></li>`;
+    for (let i = 0; i < pageCount; i++) {
+        html += `<li class="page-item ${selectedPage === (i + 1) ? 'active' : ''}"><a class="page-link" href="javascript:void(0);">${i + 1}</a></li>`;
+    }
+    html += `<li class="page-item"><a class="page-link" href="javascript:void(0);">Next</a></li>`;
+    $('.pagination').html(html);
+    $('.page-item .page-link').on('click', (eventData) => eventData.preventDefault());
+    if (selectedPage === 1) {
+        $('.page-item:first-child').addClass('disabled');
+    }
+    else if (selectedPage === pageCount) {
+        $('.page-item:last-child').addClass('disabled');
+    }
+    $('.page-item:first-child').on('click', () => navigateToPage(selectedPage - 1));
+    $('.page-item:last-child').on('click', () => navigateToPage(selectedPage + 1));
+    $('.page-item:not(.page-item:first-child, .page-item:last-child)').on('click', function () {
+        navigateToPage(+$(this).text());
+    });
+}
+/* pagination navigation */
+function navigateToPage(page) {
+    if (page < 1 || page > pageCount)
+        throw 'Invalid Page Number';
+    selectedPage = page;
+    loadAllItems();
+}
+/* hide tha pagination whenever needed */
+function showOrHidePagination() {
+    pageCount > 1 ? $('.pagination').show() : $('.pagination').hide();
 }
 export {};
