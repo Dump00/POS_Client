ee8639fdc75bd69b6f831c978f0eaa91ec9f5113
cisco <sovisrushain@gmail.com>
2021-10-12 17:38:50 +0530

    Get the item count and display in the card

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,13 @@
 "use strict";
 const BASE_API = 'http://localhost:8080/pos';
 const CUSTOMER_SERVICE_API = `${BASE_API}/customers`;
+const ITEM_SERVICE_API = `${BASE_API}/items`;
 let totalCustomers = 0;
-loadDetails();
-function loadDetails() {
+let totalItems = 0;
+loadNewCustomerCount();
+loadNewItemsCount();
+/* get the customer count */
+function loadNewCustomerCount() {
     const http = new XMLHttpRequest();
     http.onreadystatechange = () => {
         if (http.readyState === http.DONE) {
@@ -16,3 +20,20 @@ function loadDetails() {
     http.open('GET', CUSTOMER_SERVICE_API + `?page=${1}&size=${6}`, true);
     http.send();
 }
+;
+/* get the item count */
+function loadNewItemsCount() {
+    console.log('working');
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+        if (http.readyState === http.DONE) {
+            if (http.status !== 200)
+                return;
+            totalItems = +(http.getResponseHeader('X-Total-Count'));
+            $('#item-count').html(totalItems + '');
+        }
+    };
+    http.open('GET', ITEM_SERVICE_API + `?page=${1}&size=${6}`, true);
+    http.send();
+}
+;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 const BASE_API = 'http://localhost:8080/pos';
 const CUSTOMER_SERVICE_API = `${BASE_API}/customers`;
+const ITEM_SERVICE_API = `${BASE_API}/items`;
+
 
 let totalCustomers = 0;
+let totalItems = 0;
 
-loadDetails();
 
-function loadDetails(): void {
+loadNewCustomerCount();
+loadNewItemsCount();
+
+
+/* get the customer count */
+
+function loadNewCustomerCount(): void {
 
     const http = new XMLHttpRequest();
 
@@ -21,7 +29,30 @@ function loadDetails(): void {
     http.open('GET', CUSTOMER_SERVICE_API + `?page=${1}&size=${6}`, true);
 
     http.send();
-}
+};
+
+/* get the item count */
+
+function loadNewItemsCount(): void {
+
+    console.log('working');
+    
+
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = () => {
+        if(http.readyState === http.DONE){
+            if(http.status !== 200) return;
+            totalItems = +(http.getResponseHeader('X-Total-Count'))!;
+            $('#item-count').html(totalItems+'');
+        }
+    }
+
+    http.open('GET', ITEM_SERVICE_API + `?page=${1}&size=${6}`, true);
+
+    http.send();
+};
+
 
 
 
